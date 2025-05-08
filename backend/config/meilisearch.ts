a5fe89e5cad0a07695be5aeeef131e09dd74de2e
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-05-08 08:13:53 +0530

    Updated

--- backend/config/meilisearch.ts
+++ backend/config/meilisearch.ts
@@ -1,16 +1,35 @@
 // export default {
-//   indexes: {
-//     movies: {
-//       indexName: 'movies',
-//       collection: 'api::movie.movie',
-//       transformEntry({ entry }) {
-//         return {
-//           id: entry.id,
-//           title: entry.title,
-//           director: entry.director,
-//           genre: entry.genre,
-//         };
+//     indexes: {
+//       movies: {
+//         indexName: 'movies',
+//         collection: 'api::movie.movie',
+//         transformEntry({ entry }) {
+//           return {
+//             id: entry.id,
+//             title: entry.title,
+//             director: entry.director,
+//             genre: entry.genre,
+            
+//           };
+//         },
 //       },
 //     },
-//   },
-// };
+//   };
+module.exports = {
+    'meilisearch': {
+      config: {
+        host: 'https://meilisearch-production-cf2e.up.railway.app',
+        apiKey: 'kirritharun05', // Optional if your Strapi reads from .env
+        indexes: [
+          {
+            name: 'movies', // Meilisearch index name
+            contentType: 'api::movie.movie', // Strapi content type
+            settings: {
+              searchableAttributes: ['title', 'director', 'genre'],
+              displayedAttributes: ['title', 'director', 'genre'],
+            },
+          },
+        ],
+      },
+    },
+  };
